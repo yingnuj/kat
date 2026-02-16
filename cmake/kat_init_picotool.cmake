f120178e0aa799800a92ce1c52ebb70398861775
yingnuj <50822551+yingnuj@users.noreply.github.com>
2026-02-16 14:58:24 +0800

    feat: picotool external project

---
+++ cmake/kat_init_picotool.cmake
@@ -0,0 +1,47 @@
+function(kat_init_picotool)
+  find_program(picotool_EXECUTABLE picotool
+               HINTS ${CMAKE_SOURCE_DIR}/tools/picotool)
+  if(NOT picotool_EXECUTABLE AND NOT TARGET picotool)
+    message(
+      "picotool not found - configuring to build and install picotool from source"
+    )
+
+    set(picotool_BUILD_TARGET picotoolBuild)
+    set(picotool_TARGET picotool)
+
+    set(picotool_SOURCE_DIR ${CMAKE_SOURCE_DIR}/third_party/picotool)
+    set(picotool_BINARY_DIR ${CMAKE_BINARY_DIR}/picotool/picotool-build)
+    set(picotool_INSTALL_DIR ${CMAKE_SOURCE_DIR}/tools)
+
+    add_custom_target(
+      picotoolForceReconfigure
+      ${CMAKE_COMMAND} -E touch_nocreate "${CMAKE_SOURCE_DIR}/CMakeLists.txt"
+      VERBATIM)
+
+    ExternalProject_Add(
+      ${picotool_BUILD_TARGET}
+      PREFIX picotool
+      SOURCE_DIR ${picotool_SOURCE_DIR}
+      BINARY_DIR ${picotool_BINARY_DIR}
+      INSTALL_DIR ${picotool_INSTALL_DIR}
+      CMAKE_ARGS "--no-warn-unused-cli"
+                 "-DCMAKE_MAKE_PROGRAM:FILEPATH=${CMAKE_MAKE_PROGRAM}"
+                 "-DPICO_SDK_PATH:FILEPATH=${PICO_SDK_PATH}"
+                 "-DPICOTOOL_NO_LIBUSB=1"
+                 "-DPICOTOOL_FLAT_INSTALL=1"
+                 "-DCMAKE_INSTALL_PREFIX=${picotool_INSTALL_DIR}"
+                 "-DCMAKE_RULE_MESSAGES=OFF" # quieten the build
+                 "-DCMAKE_INSTALL_MESSAGE=NEVER" # quieten the install
+      BUILD_ALWAYS 1 # force dependency checking
+      EXCLUDE_FROM_ALL TRUE)
+
+    set(picotool_EXECUTABLE ${picotool_INSTALL_DIR}/picotool/picotool)
+    add_executable(${picotool_TARGET} IMPORTED GLOBAL)
+    set_property(TARGET ${picotool_TARGET} PROPERTY IMPORTED_LOCATION
+                                                    ${picotool_EXECUTABLE})
+    add_dependencies(${picotool_TARGET} ${picotool_BUILD_TARGET})
+  else()
+    message(
+      "picotool found at ${picotool_EXECUTABLE} - skipping picotool build")
+  endif()
+endfunction(kat_init_picotool)
